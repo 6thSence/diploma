8f580f0285280d6a6ba28986ee98e3cf2df02425
6thSence <d.pushkarskaya.pr@gmail.com>
2016-05-15 18:53:45 +0600

    hot reload EAH

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,22 @@ const short = require('postcss-short');
 const reporter = require('postcss-browser-reporter');
 const stylelint = require('stylelint');
 const rulesStyles = require('./stylelintrc.json');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/entry.js",
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/entry'
+    ],
     output: {
         path: __dirname,
-        filename: "./dist/bundle.js"
+        filename: './dist/bundle.js'
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [
             {
@@ -19,7 +28,7 @@ module.exports = {
                 loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]!postcss-loader',
                 exclude: /node_modules/
             },
-            { test: /.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /.js?$/, loaders: ['react-hot', 'babel-loader'], exclude: /node_modules/ },
             {
                 test: /\.json$/,
                 loader: 'json-loader'
