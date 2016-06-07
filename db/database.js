6e8a71c7f2a79a4d09985acb7527fcab6052028b
6thSence <d.pushkarskaya.pr@gmail.com>
2016-06-07 23:36:14 +0600

    MONGODB_URI

--- db/database.js
+++ db/database.js
@@ -1,7 +1,8 @@
 const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
-const url = 'mongodb://6thsence:pushgroupnsk17@ds025263.mlab.com:25263/heroku_sr0fbvrw';
+//const url = 'mongodb://6thsence:pushgroupnsk17@ds025263.mlab.com:25263/heroku_sr0fbvrw';
+const url = process.env.MONGODB_URI;
 
 module.exports.connect = () =>
     new Promise((resolve, reject) => {
