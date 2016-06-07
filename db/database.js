23d0673b2ccc2b40c2c58bb8e89f82f42fc7c294
6thSence <d.pushkarskaya.pr@gmail.com>
2016-06-07 23:44:22 +0600

    MONGODB_URI save password for connect

--- db/database.js
+++ db/database.js
@@ -1,7 +1,6 @@
 const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
-//const url = 'mongodb://6thsence:pushgroupnsk17@ds025263.mlab.com:25263/heroku_sr0fbvrw';
 const url = process.env.MONGODB_URI;
 
 module.exports.connect = () =>
