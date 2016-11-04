06a58da30a66b74f4638d6655f5a46db98371f48
Daria <6thSence@users.noreply.github.com>
2016-11-04 20:15:11 +0300

    change reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -2,17 +2,16 @@ import { AUTH, ADD_RESULT } from '../constants/user';
 import { tail } from '../utils/helpers';
 
 const initialState = {
-    "id": 'id',
-    "name": "name",
-    "email": "email",
-    "password": "password",
-    "authAnswer": false
+    'id': 'id',
+    'name': 'name',
+    'email': 'email',
+    'password': 'password',
+    'authAnswer': false
 };
 
 export default function user(state = initialState, action) {
     switch (action.type) {
         case AUTH:
-            console.log(action.users);
             const users = action.users.filter(item => item.email == action.userEmail && item.password == action.userPassword);
             return users.length > 0 ? { ...tail(users), authAnswer: true } : { authAnswer: false};
         case ADD_RESULT:
