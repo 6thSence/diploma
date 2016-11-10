6ffc1ce1120c8e7af035f361b712cd7ac5d9a64c
Daria <6thSence@users.noreply.github.com>
2016-11-10 22:48:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 const app = isDevelopment ? require('./app.dev.js') : require('./app.prod.js');
 const port = process.env.PORT || 3000;
 
-app.listen(port, function(error) {
+app.listen(port, error => {
     if (error) {
         console.error(error);
     } else {
